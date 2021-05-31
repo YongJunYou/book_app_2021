7c9ca109ecef7c553fea1ef5f40f171d8d3fcc9f
YYJ <dbys34@gmail.com>
2021-05-31 23:28:41 +0900

    Updates

---
+++ precache-manifest.dda98e52a89068f3793aa38c8b2bd9d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa854282561cac8053768a8038216d57",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "5e3cc374b74947b112ef",
+    "url": "/movie_app_2019/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "acc9b49fca3d8036abcb",
+    "url": "/movie_app_2019/static/js/2.a8ec8f9f.chunk.js"
+  },
+  {
+    "revision": "5e3cc374b74947b112ef",
+    "url": "/movie_app_2019/static/js/main.081445d3.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
