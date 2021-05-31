fff892f34f6c40a002ae202ef6804ac1059537b4
YYJ <dbys34@gmail.com>
2021-05-31 22:52:06 +0900

    123

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getBooks = async() =>{
+	  axios.get("https://dapi.kakao.com/v2/search/web",
+        {data: {query: "K"}},
+        {headers: {Authorization: `KakaoAK dfa03086c4a2134d8fe234a777eaa281`}})
+        .then(function (response) {
+            console.log(response.documents[0].title);
+            console.log(response.documents[0].thumbnail);})
+        };
+
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+    this.getBooks();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default App;
